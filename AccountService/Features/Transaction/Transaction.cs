c93cadf6319600271f305cc35aede5170416ecad
holo2k <nneketaa@yandex.ru>
2025-07-29 02:11:37 +0500

    fix: nullable counterpartyAccountId

--- AccountService/Features/Transaction/Transaction.cs
+++ AccountService/Features/Transaction/Transaction.cs
@@ -4,7 +4,7 @@ public class Transaction
 {
     public Guid Id { get; set; }
     public Guid AccountId { get; set; }
-    public Guid CounterPartyAccountId { get; set; }
+    public Guid? CounterPartyAccountId { get; set; }
     public decimal Amount { get; set; }
     public string Currency { get; set; } = string.Empty;
     public TransactionType Type { get; set; }
--- AccountService/Features/Transaction/TransactionDto.cs
+++ AccountService/Features/Transaction/TransactionDto.cs
@@ -7,7 +7,7 @@ public class TransactionDto
     public Guid AccountId { get; set; }
 
     // ReSharper disable once UnusedMember.Global (Используется в auto mapper)
-    public Guid CounterPartyAccountId { get; set; }
+    public Guid? CounterPartyAccountId { get; set; }
     public decimal Amount { get; set; }
     public string Currency { get; set; } = string.Empty;
     public TransactionType Type { get; set; }
