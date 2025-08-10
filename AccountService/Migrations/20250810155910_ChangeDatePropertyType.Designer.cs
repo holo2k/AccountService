d35bec34d14be706c43058be71062d58b0963cef
holo2k <nneketaa@yandex.ru>
2025-08-10 21:08:59 +0500

    task3: db migrations

--- AccountService/Migrations/20250810155910_ChangeDatePropertyType.Designer.cs
+++ AccountService/Migrations/20250810155910_ChangeDatePropertyType.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AccountService.Infrastructure.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AccountService.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250810155910_ChangeDatePropertyType")]
+    partial class ChangeDatePropertyType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
